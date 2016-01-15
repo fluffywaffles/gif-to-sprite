a7711fcdacead4040c793abd9eab71b6b690430a
skorlir <skorlir@gmail.com>
2016-01-14 23:08:37 -0600

    Add __pycache__ to .gitignore and clean up argparser line lengths

--- gif_to_sprite.py
+++ gif_to_sprite.py
@@ -19,13 +19,17 @@ def gif_to_sprite (gif=None, mastername=None, cleanup=True):
             os.remove(f)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Make a spritesheet from an animated gif.')
+    parser = argparse.ArgumentParser(
+        description='Make a spritesheet from an animated gif.'
+    )
     parser.add_argument('gif', metavar='gif', type=str, nargs=1)
-    parser.add_argument('mastername', help='Output spritesheet name (w/ extension)', metavar='', type=str, nargs='?', default=None)
-    parser.add_argument('-m', '--no-cleanup', dest='cleanup', help='leave a mess / don\'t clean up frames', action='store_false')
+    parser.add_argument('mastername',
+                        help='Output spritesheet name (w/ extension)',
+                        metavar='', type=str, nargs='?', default=None)
+    parser.add_argument('-m', '--no-cleanup', dest='cleanup',
+                        help='leave a mess / don\'t clean up frames',
+                        action='store_false')
 
     args = parser.parse_args()
-
-    print (args)
 
     gif_to_sprite(gif=args.gif, mastername=args.mastername, cleanup=args.cleanup)
